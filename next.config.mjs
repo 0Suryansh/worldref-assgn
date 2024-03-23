05cdb27f91d57b3df7b2829d1a699d8d50ceaf33
Suryansh <75418521+0Suryansh@users.noreply.github.com>
2024-03-23 13:57:01 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
+    output: "export",
     images: {
         domains: ['lh3.googleusercontent.com', 'successive.cloud', 'play-lh.googleusercontent.com', 'encrypted-tbn0.gstatic.com', 'imgv3.fotor.com', 'picsum.photos'],
       },
